36b515c63730a9b848fd38668b4c3040c094859f
Jacob Errington <git@mail.jerrington.me>
2021-12-05 16:29:20 -0500

    2021 day 3: reformat

--- y2021/src/day3p1.rs
+++ y2021/src/day3p1.rs
@@ -3,11 +3,17 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn vec_transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
-where T: Clone {
-    if v.is_empty() { return Vec::new(); }
+where
+    T: Clone,
+{
+    if v.is_empty() {
+        return Vec::new();
+    }
     let len = v[0].len();
 
-    (0..len).map(|i| v.iter().map(|r| r[i].clone()).collect()).collect()
+    (0..len)
+        .map(|i| v.iter().map(|r| r[i].clone()).collect())
+        .collect()
 }
 
 #[derive(Clone)]
@@ -48,29 +54,47 @@ trait BitArray {
     fn collapse(&self) -> i32;
 
     /// Flips all bits in the array.
-    fn flipped(&self) -> Self;
+    fn flip(&self) -> Self;
 }
 
 impl BitArray for Vec<Bit> {
     fn most_common_bit(&self) -> Bit {
-        if self.iter().map(|x| x.to_weight()).sum::<i32>() >= 0 { Bit::I } else { Bit:: O }
+        if self.iter().map(|x| x.to_weight()).sum::<i32>() >= 0 {
+            Bit::I
+        } else {
+            Bit::O
+        }
     }
 
     fn collapse(&self) -> i32 {
-        self.iter().rev().zip(0 ..).map(|(b, k)| if let Bit::I = b { 2i32.pow(k) } else { 0 }).sum()
+        self.iter()
+            .rev()
+            .zip(0..)
+            .map(|(b, k)| if let Bit::I = b { 2i32.pow(k) } else { 0 })
+            .sum()
     }
 
-    fn flipped(&self) -> Self {
+    fn flip(&self) -> Self {
         self.iter().map(|x| x.flip()).collect()
     }
 }
 
-
 fn main() {
     let file = File::open(Path::new("input/day3.txt")).unwrap();
-    let matrix: Vec<Vec<Bit>> = io::BufReader::new(file).lines().map(|line| line.unwrap().chars().map(Bit::parse).collect()).collect();
+    let matrix: Vec<Vec<Bit>> = io::BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().chars().map(Bit::parse).collect())
+        .collect();
     let tr_matrix = vec_transpose(matrix);
-    let gamma = tr_matrix.iter().map(|x| x.most_common_bit()).collect::<Vec<_>>().collapse();
-    let epsilon = tr_matrix.iter().map(|x| x.flipped().most_common_bit()).collect::<Vec<_>>().collapse();
+    let gamma = tr_matrix
+        .iter()
+        .map(|x| x.most_common_bit())
+        .collect::<Vec<_>>()
+        .collapse();
+    let epsilon = tr_matrix
+        .iter()
+        .map(|x| x.flip().most_common_bit())
+        .collect::<Vec<_>>()
+        .collapse();
     println!("{} * {} = {}", gamma, epsilon, gamma * epsilon);
 }
