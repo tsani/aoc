947313d7737ae670bb7b71e879f182dacd4eb13e
Jacob Errington <git@mail.jerrington.me>
2022-12-09 13:14:51 -0500

    2022 complete day 8

--- y2022/app/Day8.hs
+++ y2022/app/Day8.hs
@@ -2,11 +2,16 @@
 
 module Day8 where
 
+import Control.Comonad
+import Data.Bifunctor
+import Data.Foldable
 import qualified Data.Vector as V
 
 newtype Grid a = Grid { unGrid :: V.Vector (V.Vector a) }
   deriving (Functor, Foldable, Traversable)
 
+type Score = Int
+type Count = Int
 type Height = Int
 type Loc = (Int, Int)
 
@@ -17,10 +22,21 @@ data GridZ a = GridZ
   }
   deriving (Functor, Foldable, Traversable)
 
+instance Comonad GridZ where
+  extract = here
+  duplicate g = g' where
+    g' = GridZ
+      { here = g
+      , index = index g
+      , dir = fmap duplicate <$> dir g
+      }
+
 data Dir a = Dir { up :: a, down :: a, left :: a, right :: a }
   deriving (Functor, Foldable, Traversable, Show)
 
-dirs :: Dir (Dir a -> a)
+type Direction a = Dir a -> a
+
+dirs :: Dir (Direction a)
 dirs = Dir { up = up, down = down, left = left, right = right }
 
 instance Applicative Dir where
@@ -59,16 +75,35 @@ parse = Grid . V.fromList . map (V.fromList . map (read . pure)) . lines
 findTallestTrees :: GridZ Height -> GridZ (Dir Height, Height)
 findTallestTrees g = g' where
   g' = GridZ
-    { here = (collapse (dir g'), here g)
+    { here = (collapse (fmap here <$> dir g'), here g)
     , index = index g
     , dir = fmap findTallestTrees <$> dir g
     }
-  f access (d, h) = max (access d) h
-  collapse x = ((\d -> maybe (-1) (f d)) <$> dirs) <*> (fmap here <$> x)
+  collapse x = (\d -> maybe (-1) (uncurry max . first d)) <$> dirs <*> x
 
+-- | Look in each direction from the given point and return a list of elements
+-- seen in each direction.
+lookEverywhere :: GridZ a -> Dir [a]
+lookEverywhere g = f <$> dirs <*> dir g where
+  f d = maybe [] (\g -> here g : f d (d . dir $ g))
+
+-- | Calculates for each tree T a 4-tuple indicating for each direction
+-- how many trees that T can see before the edge or a tree of equal or greater height.
+findViewingDistances :: GridZ Height -> GridZ (Dir Count)
+findViewingDistances = extend k where
+  k :: GridZ Height -> Dir Count
+  k g = length . takeWhile1 (< here g) <$> lookEverywhere g where
+    takeWhile1 p l = case span p l of
+      (xs, []) -> xs -- when we can see all the way to the edge
+      (xs, x : _) -> x : xs -- when we run into a tree, we need to count it!
+
+scenicScore :: Dir Count -> Score
+scenicScore = product
+
+-- | A tree is visible if it is taller than the tallest tree in at least one
+-- direction.
 visibleTrees :: GridZ (Dir Height, Height) -> GridZ Bool
-visibleTrees = fmap $ \(d, h) ->
-  any (\dir -> h > dir d) [up, down, left, right]
+visibleTrees = fmap $ \(d, h) -> any id $ (h >) <$> d
 
 materialize :: GridZ a -> [[a]]
 materialize = rows . seekUp . seekLeft where
@@ -78,8 +113,12 @@ materialize = rows . seekUp . seekLeft where
   row g = here g : maybe [] row (right . dir $ g)
   rows g = row g : maybe [] rows (down . dir $ g)
 
+answer1, answer2 :: String -> Int
+answer1 =
+  sum . map (length . filter id) . materialize . visibleTrees . findTallestTrees . gridz (0, 0) . parse
+
+answer2 =
+  maximum . fmap maximum . materialize . fmap scenicScore . findViewingDistances . gridz (0, 0) . parse
+
 main :: IO ()
-main =
-  print
-  =<< sum . map (length . filter id) . materialize . visibleTrees . findTallestTrees . gridz (0, 0) . parse
-  <$> readFile "input/day8.txt"
+main = print =<< answer2 <$> readFile "input/day8.txt"
